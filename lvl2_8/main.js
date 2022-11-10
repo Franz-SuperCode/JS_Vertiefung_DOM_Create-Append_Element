0acdd48ade006f04ceb88f4a2daf6e9e77248f34
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-10 12:49:38 +0100

    Add Enter

--- lvl2_8/main.js
+++ lvl2_8/main.js
@@ -1,11 +1,26 @@
 let buttonElement = document.querySelector("#enter");
 let ulElement = document.querySelector("ul");
 
+//Wenn der Knopf gedrückt wird, soll eine neue Liste hinzugefügt werden
 buttonElement.addEventListener("click", () => {
-    let inputValue = document.querySelector("#userinput").value;
-    console.log(inputValue);
+    setNewList();
+})
 
+//Wenn Enter gedrückt wird, soll eine neue Liste hinzugefügt werden
+document.addEventListener("keydown", (event) => {
+    let key = event.key;
+
+    if (key === "Enter") {
+        setNewList();
+        console.log(key);
+    }
+})
+
+
+function setNewList() {
+    let inputValue = document.querySelector("#userinput").value;
     let newLi = document.createElement("li");
+
     newLi.textContent = inputValue;
     ulElement.appendChild(newLi);
-})
+}
